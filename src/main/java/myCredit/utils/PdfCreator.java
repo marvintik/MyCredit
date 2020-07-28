488853e2695897bc01c675f75fe362d9b8c7b965
Olena Haladzhii <marvintikln@gmail.com>
2020-07-28 21:57:45 +0300

    update_pdf

--- src/main/java/myCredit/utils/PdfCreator.java
+++ src/main/java/myCredit/utils/PdfCreator.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
@@ -21,73 +22,67 @@ import static myCredit.utils.PrintCredit.*;
 @Component
 public class PdfCreator {
     public static final String FONT = "./src/main/resources/fonts/tnr2.ttf";
+    private BaseFont bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+    private Font titleFont = new Font(bf, 30, Font.BOLDITALIC);
+    private Font bigFont = new Font(bf, 16, Font.BOLD);
+    private Font smallFont = new Font(bf, 12, Font.NORMAL);
+
+
+    public PdfCreator() throws IOException, DocumentException {
+    }
 
     @SneakyThrows
-    public ByteArrayInputStream createPersonPdf(Person person){
-        String name = person.getName() +"_credits.pdf";
-        Document document = new Document();
-        BaseFont bf=BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font titleFont = new Font(bf,30,Font.BOLDITALIC);
-        Font bigFont=new Font(bf,16,Font.BOLD);
-        Font smallFont=new Font(bf,12,Font.NORMAL);
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        PdfWriter writer = PdfWriter.getInstance(document, out);
-        document.open();
-        document.addTitle(name);
+    public ByteArrayInputStream createPersonPdf(Person person) {
         var title = new Paragraph(person.getName(), titleFont);
-        title.setAlignment(Element.ALIGN_CENTER);
-        document.add(title);
         java.util.List<Credit> credits = person.getCredits();
-        document.add(new Paragraph("Общая сумма: "+printSum(credits) + "грн", bigFont));
-        document.add(new Paragraph("Сумма в месяц: "+printMonthPay(credits) + "грн", bigFont));
-        addImage(document, person);
-        addTable(document, credits, bigFont, smallFont);
-        document.close();
-        writer.close();
-        return new ByteArrayInputStream(out.toByteArray());
+        String name = person.getName() + "_credits.pdf";
+        Image image;
+        if (person.getImage().isEmpty()) {
+            image = Image.getInstance(person.getImageFile());}
+        else {
+            image = Image.getInstance(new URL(person.getImage()));}
+
+        return getInputStreamDocument(title, credits, name, image);
     }
 
-    @SneakyThrows
-    public ByteArrayInputStream createUserPdf(User user){
-        String name = user.getUsername() +"_credits.pdf";
+    private ByteArrayInputStream getInputStreamDocument(Paragraph title, List<Credit> credits, String name, Image image) throws DocumentException {
         Document document = new Document();
-        BaseFont bf=BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-        Font titleFont = new Font(bf,30,Font.BOLDITALIC);
-        Font bigFont=new Font(bf,16,Font.BOLD);
-        Font smallFont=new Font(bf,12,Font.NORMAL);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         PdfWriter writer = PdfWriter.getInstance(document, out);
         document.open();
         document.addTitle(name);
-        var title = new Paragraph(user.getUsername(), titleFont);
         title.setAlignment(Element.ALIGN_CENTER);
         document.add(title);
-        java.util.List<Credit> credits = printCredits(user);
-        document.add(new Paragraph("Общая сумма: "+printSum(credits) + "грн", bigFont));
-        document.add(new Paragraph("Сумма в месяц: "+printMonthPay(credits) + "грн", bigFont));
+        document.add(new Paragraph("Общая сумма: " + printSum(credits) + "грн", bigFont));
+        document.add(new Paragraph("Сумма в месяц: " + printMonthPay(credits) + "грн", bigFont));
+        if (image != null) addImage(document, image);
         addTable(document, credits, bigFont, smallFont);
         document.close();
         writer.close();
         return new ByteArrayInputStream(out.toByteArray());
     }
 
+
     @SneakyThrows
-    public void addImage(Document document, Person person){
-        if(person.getImage().isEmpty()){
-        Image image1 = Image.getInstance(person.getImageFile());
-        image1.scaleAbsolute(140, 140);
-        image1.setAlignment(Element.ALIGN_RIGHT);
-        document.add(image1);
-        }
-        else{
-        Image image2 = Image.getInstance(new URL(person.getImage()));
-        image2.scaleAbsolute(140,140);
-        image2.setAlignment(Element.ALIGN_RIGHT);
-        document.add(image2);}
+    public ByteArrayInputStream createUserPdf(User user) {
+        String name = user.getUsername() + "_credits.pdf";
+        var title = new Paragraph(user.getUsername(), titleFont);
+        java.util.List<Credit> credits = printCredits(user);
+        Image image = Image.getInstance("src/main/resources/static/img/avatar1.png");
+        if (user.getPicture()!= null){
+       image = Image.getInstance(new URL(user.getPicture()));}
+        return getInputStreamDocument(title, credits, name, image);
     }
 
     @SneakyThrows
-    public void addTable(Document document, List<Credit> credits, Font bigFont, Font smallFont){
+    public void addImage(Document document, Image image) {
+            image.scaleAbsolute(140, 140);
+            image.setAlignment(Element.ALIGN_RIGHT);
+            document.add(image);
+    }
+
+    @SneakyThrows
+    public void addTable(Document document, List<Credit> credits, Font bigFont, Font smallFont) {
         PdfPTable table = new PdfPTable(7);
         table.setWidthPercentage(100); //Width 100%
         table.setSpacingBefore(10f); //Space before table
@@ -96,7 +91,7 @@ public class PdfCreator {
 
         table.addCell(new PdfPCell(new Phrase("#", bigFont)));
         table.addCell(new PdfPCell(new Phrase("Банк", bigFont)));
-        table.addCell( new PdfPCell(new Phrase("Название", bigFont)));
+        table.addCell(new PdfPCell(new Phrase("Название", bigFont)));
         table.addCell(new PdfPCell(new Phrase("Дата начала", bigFont)));
         table.addCell(new PdfPCell(new Phrase("Дата окончания", bigFont)));
         table.addCell(new PdfPCell(new Phrase("Сумма", bigFont)));
@@ -104,7 +99,7 @@ public class PdfCreator {
 
         table.setHeaderRows(1);
         int count = 1;
-        for(Credit credit : credits){
+        for (Credit credit : credits) {
             table.addCell(new PdfPCell(new Phrase(String.valueOf(count), smallFont)));
             count++;
             table.addCell(new PdfPCell(new Phrase(credit.getBank(), smallFont)));
